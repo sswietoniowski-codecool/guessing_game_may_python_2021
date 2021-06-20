355a78bc285392bff97281ab0777245287a2815a
Kamczata <kam.czernek@gmail.com>
2021-06-20 11:30:08 +0200

    added function prepare_random_numbers

--- guessing_game.py
+++ guessing_game.py
@@ -4,28 +4,34 @@ import random
 
 def play_game(numbers, min_value, max_value):
     for i in range(10):
-        guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
+        guessed_number = int(
+            input(f"Enter an integer from {min_value} to {max_value}: "))
         while numbers[i] != guessed_number:
             if guessed_number < numbers[i]:
                 print("guess is low")
-                guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
+                guessed_number = int(
+                    input(f"Enter an integer from {min_value} to {max_value}: "))
             elif guessed_number > numbers[i]:
                 print("guess is high")
-                guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
+                guessed_number = int(
+                    input(f"Enter an integer from {min_value} to {max_value}: "))
             else:
                 break
         print("you guessed it!")
 
 
-def main():
+def prepare_random_numbers(count, min_value, max_value):
     numbers = []
-    for _ in range(10):
-        numbers.append(random.randint(1, 99))
+    for _ in range(count):
+        numbers.append(random.randint(min_value, max_value))
+    return numbers
+
+
+def main():
+    numbers = prepare_random_numbers(10, 1, 99)
     play_game(numbers, 1, 99)
 
-    numbers = []
-    for _ in range(10):
-        numbers.append(random.randint(1, 49))
+    numbers = prepare_random_numbers(10, 1, 49)
     play_game(numbers, 1, 49)
 
 
