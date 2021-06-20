216f02fda0aa8b9f6da6e3ae925212a7d7fd2982
Nessquik <asiamarcinkowska@gmail.com>
2021-06-20 11:05:32 +0200

    simplified by using for loop

--- guessing_game.py
+++ guessing_game.py
@@ -4,16 +4,9 @@ import random
 
 def main():
     a = []
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
-    a.append(random.randint(1, 99))
+    for _ in range(10):
+        a.append(random.randint(1, 99))
+
     for i in range(10):
         g = int(input("Enter an integer from 1 to 99: "))
         while a[i] != g:
@@ -28,16 +21,9 @@ def main():
         print("you guessed it!")
 
     b = []
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
-    b.append(random.randint(1, 49))
+    for _ in range(10):
+        b.append(random.randint(1, 49))
+
     for i in range(10):
         g = int(input("Enter an integer from 1 to 49: "))
         while b[i] != g:
