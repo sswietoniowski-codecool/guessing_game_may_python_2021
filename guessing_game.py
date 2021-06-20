bdf20fa46190e2e4d1260d6308c88ca9f5d1cc5a
CecyliaM <cecylia.meredith@gmail.com>
2021-06-20 11:21:39 +0200

    simplified main function

--- guessing_game.py
+++ guessing_game.py
@@ -2,43 +2,31 @@ import math
 import random
 
 
-def play_game(numbers):
+def play_game(numbers, min_value, max_value):
     for i in range(10):
-        guessed_number = int(input("Enter an integer from 1 to 99: "))
+        guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
         while numbers[i] != guessed_number:
             if guessed_number < numbers[i]:
                 print("guess is low")
-                guessed_number = int(input("Enter an integer from 1 to 99: "))
+                guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
             elif guessed_number > numbers[i]:
                 print("guess is high")
-                guessed_number = int(input("Enter an integer from 1 to 99: "))
+                guessed_number = int(input(f"Enter an integer from {min_value} to {max_value}: "))
             else:
                 break
         print("you guessed it!")
+
 
 def main():
     numbers = []
     for _ in range(10):
         numbers.append(random.randint(1, 99))
-    play_game(numbers)
-    
+    play_game(numbers, 1, 99)
 
-    b = []
+    numbers = []
     for _ in range(10):
-        b.append(random.randint(1, 49))
-
-    for i in range(10):
-        g = int(input("Enter an integer from 1 to 49: "))
-        while b[i] != g:
-            if g < b[i]:
-                print("guess is low")
-                g = int(input("Enter an integer from 1 to 49: "))
-            elif g > b[i]:
-                print("guess is high")
-                g = int(input("Enter an integer from 1 to 49: "))
-            else:
-                break
-        print("you guessed it!")
+        numbers.append(random.randint(1, 49))
+    play_game(numbers, 1, 49)
 
 
 if __name__ == "__main__":
